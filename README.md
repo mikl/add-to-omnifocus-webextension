03c92784911d65140e8d8e11270e1407bce2609e
Mikkel Hoegh <mikkel@hoegh.org>
2020-04-18 13:00:38 +0200

    feat: create “Add to OmniFocus” browser action

---
+++ README.md
@@ -0,0 +1,9 @@
+# Add to OmniFocus WebExtension
+
+## Browser extension to add web pages to OmniFocus
+
+Adds a button to the browser toolbar that adds the current page as a task in
+OmniFocus.
+
+Intended to work in all browsers that support web extensions. Currently only
+tested in Firefox.
---
+++ src/background.js
@@ -0,0 +1,41 @@
+function composeOmniFocusAddTaskUrl(parameters) {
+  const parts = []
+
+  for (const [key, value] of Object.entries(parameters)) {
+    parts.push(`${key}=${encodeURIComponent(value)}`)
+  }
+
+  return `omnifocus:///add?${parts.join('&')}`
+}
+
+async function handleBrowserActionClick() {
+  const tabs = await browser.tabs.query({
+    active: true,
+    currentWindow: true
+  })
+
+  if (tabs.length > 0) {
+    const activeTab = tabs[0]
+
+    const addTaskUrl = composeOmniFocusAddTaskUrl(
+      {
+        name: activeTab.title,
+        note: activeTab.url
+      }
+    )
+
+    await openOmniFocusUrl(addTaskUrl)
+
+    console.info(`Added task “${activeTab.title}” to OmniFocus.`)
+  } else {
+    console.error('Browser tabs query returned empty set, cannot add OmniFocus task.')
+  }
+}
+
+function openOmniFocusUrl(url) {
+  // This is a little gross, but I haven’t been able to find a neater way to
+  // open the OmniFocus URL, and this appears to have no side effects.
+  location = url
+}
+
+browser.browserAction.onClicked.addListener(handleBrowserActionClick)
